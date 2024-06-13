4b32a0d054551a9365b0699011361bf24a78dde2
wassim derouiche <wassim.derouiche@dauphine.eu>
2024-06-13 21:23:23 +0200

    fixed delete request

--- src/main/java/com/dauphine/blogger/config/WebConfig.java
+++ src/main/java/com/dauphine/blogger/config/WebConfig.java
@@ -9,6 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 @Override
 public void addCorsMappings(CorsRegistry registry) {
-registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true);
 }
 }
